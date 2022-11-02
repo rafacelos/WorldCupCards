9ff1dce79d4945b622e3edec8a2c0b8dbb5da25b
Rafael Oliveira <rafa.celos.dev@gmail.com>
2022-11-02 02:41:15 +0000

    Finished

--- src/Home/styles.ts
+++ src/Home/styles.ts
@@ -31,5 +31,15 @@ export const styles = StyleSheet.create({
     fontWeight: '900',
     color: '#000',
     textTransform: 'uppercase'
-  }
+  },
+  sticker: {
+    backgroundColor: '#373D98'
+  },
+  retry: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    marginTop: 22,
+    marginBottom: 12,
+  },
 });
